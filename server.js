007922d77331f550a4fccf95d453f681076565a2
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-22 06:12:40 +0530

    Handling different url requests and routing the user

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http=require('http')
 
 const server=http.createServer((req, res)=>{
-    console.log("Vivek")
+    if (req.url==`/home`){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html><head><title>My First Page</title></head><body><h1>Welcome Home!</h1></body></html>`)
+        res.end()
+    }
+    if (req.url==`/about`){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html><head><title>My First Page</title></head><body><h1>Welcome to About Us page!</h1></body></html>`)
+        res.end()
+    }
+    if (req.url==`/`){
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`<html><head><title>My First Page</title></head><body><h1>Welcome to my Node Js Project!</h1></body></html>`)
+        res.end()
+    }
 })
 
 server.listen(4000)

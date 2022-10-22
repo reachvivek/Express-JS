d27a57f491dc6e90fae3bc79a49d97f10bd6aa9e
Vivek Singh <rogerthatvivek@gmail.com>
2022-10-22 10:31:33 +0530

    bodyParser for getting request.body

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express=require('express')
-
+const bodyParser=require('body-parser')
 const app = express();
 
-app.use((req, res, next)=>{
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product', (req, res, next)=>{
+    console.log("In the middleware!")
+    res.send(`<form method='POST' action='/product'><input placeholder='title' name='title' type='text'></input><input placeholder='size' name='size' type='text'></input><input type='submit'></input></form>`)
+    next();
+})
+
+app.post('/product', (req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+
+
+app.use('/', (req, res, next)=>{
     console.log("In the middleware!")
     res.send(`<h1>Hello World</h1>`)
     next();
 })
 
 const routes=require('./routes')
-
-const server=http.createServer(routes)
 
 app.listen(4000)

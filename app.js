83c4c86d1b56f7d2c6e67a668a5ccfe09c0cc0ad
Vivek Singh <rogerthatvivek@gmail.com>
2022-11-05 15:30:50 +0530

    Updated Add to Cart Functionality

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById("63661567f3e257a89bcc5af9")
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -4,14 +4,58 @@ const getDb=require('../util/database').getDb;
 const ObjectId=mongodb.ObjectId
 
 class User{
-  constructor(username, email){
+  constructor(username, email, cart, id){
     this.username=username;
-    this.email=email
+    this.email=email,
+    this.cart=cart,
+    this._id=id
   }
   save(){
     const db=getDb()
     return db.collection('users').insertOne(this)
   }
+
+  addToCart(product){
+    const cartProductIndex=this.cart.items.findIndex(cp=>{
+      return cp.productId.toString()==product._id.toString()
+    })
+    let newQuantity=1;
+    // const updatedCart={items:[{...product, quantity:1}]}
+    const updatedCartItems=[...this.cart.items];
+
+    if(cartProductIndex>=0){
+      newQuantity=this.cart.items[cartProductIndex].quantity+1
+      updatedCartItems[cartProductIndex].quantity=newQuantity
+    }else{
+      updatedCartItems.push({productId:new ObjectId(product._id), quantity:newQuantity})
+    }
+    const updatedCart={
+      items:updatedCartItems
+    }
+
+    const db=getDb();
+    return db.collection('users').updateOne(
+      {_id: new ObjectId(this._id)},
+      {$set:{cart:updatedCart}}
+      )
+  }
+
+  getCart(){
+    const db=getDb();
+    const productIds=this.cart.items.map(i=>i.productId)
+    return db.collection('products')
+    .find({_id:{$in:productIds}})
+    .toArray()
+    .then(products=>{
+      return products.map(p=>{
+        return {...p, quantity:this.cart.items.find(i=>{
+          return String(i.productId)===String(p._id)
+        }).quantity
+      }
+      })
+    })
+  }
+
   static findById(userId){
     const db=getDb();
     return db.collection('users').findOne({_id: new ObjectId(userId)})

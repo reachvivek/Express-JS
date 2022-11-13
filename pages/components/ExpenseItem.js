fa44674504bd48afc403532a33dc420cc165e5ad
Vivek Singh <rogerthatvivek@gmail.com>
2022-11-13 20:15:01 +0530

    Added Expense Details Component by Splitting Expense Item

--- pages/components/ExpenseItem.js
+++ pages/components/ExpenseItem.js
@@ -1,13 +1,11 @@
 import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.price}</div>
-      </div>
+      <ExpenseDetails title={props.title} price={props.price} />
     </div>
   );
 }

afc9d83d738a153f7a3d9e94fca06a7819e347c0
Vivek Singh <rogerthatvivek@gmail.com>
2022-11-14 15:25:26 +0530

    Added delete button functionality

--- pages/components/Expenses/ExpenseItem.js
+++ pages/components/Expenses/ExpenseItem.js
@@ -1,12 +1,16 @@
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function ExpenseItem(props) {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails title={props.title} price={props.price} />
+      <button className="delete-btn" onClick={props.delete}>
+        <DeleteIcon className="icon" />
+      </button>
     </Card>
   );
 }
--- pages/components/Expenses/Expenses.js
+++ pages/components/Expenses/Expenses.js
@@ -1,18 +1,27 @@
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import { useState } from "react";
 
 function Expenses() {
-  const expenses = [
-    { date: new Date(2022, 10, 10), title: "Petrol", price: "$100" },
-    { date: new Date(2022, 1, 15), title: "Food", price: "$100" },
-    { date: new Date(2023, 10, 8), title: "KFC", price: "$100" },
-    { date: new Date(2022, 10, 1), title: "Rent", price: "$100" },
-  ];
+  const [expenses, setExpenses] = useState([
+    { id: 0, date: new Date(2022, 10, 10), title: "Petrol", price: "$100" },
+    { id: 1, date: new Date(2022, 1, 15), title: "Food", price: "$100" },
+    { id: 2, date: new Date(2023, 10, 8), title: "KFC", price: "$100" },
+    { id: 3, date: new Date(2022, 10, 1), title: "Rent", price: "$100" },
+  ]);
+
+  function deleteHandler(id) {
+    const newExpenses = [...expenses].filter((expense) => expense.id !== id);
+    console.log(newExpenses);
+    setExpenses(newExpenses);
+  }
+
   let expenseItems = expenses.map((expense) => (
     <ExpenseItem
       date={expense.date}
       title={expense.title}
       price={expense.price}
+      delete={() => deleteHandler(expense.id)}
     />
   ));
   return <Card className="expenses">{expenseItems}</Card>;

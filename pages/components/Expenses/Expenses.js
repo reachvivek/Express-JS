15247f9191bcf376dcfa51f6a46b1946b7cd411f
Vivek Singh <rogerthatvivek@gmail.com>
2022-11-14 21:28:08 +0530

    Added Edit Functionality

--- pages/components/Expenses/Expenses.js
+++ pages/components/Expenses/Expenses.js
@@ -4,14 +4,25 @@ import { useState } from "react";
 
 function Expenses() {
   const [expenses, setExpenses] = useState([
-    { id: 0, date: new Date(2022, 10, 10), title: "Petrol", price: "$100" },
-    { id: 1, date: new Date(2022, 1, 15), title: "Food", price: "$100" },
-    { id: 2, date: new Date(2023, 10, 8), title: "KFC", price: "$100" },
-    { id: 3, date: new Date(2022, 10, 1), title: "Rent", price: "$100" },
+    { id: 0, date: new Date(2022, 10, 10), title: "Petrol", price: "$500" },
+    { id: 1, date: new Date(2022, 1, 15), title: "Food", price: "$300" },
+    { id: 2, date: new Date(2021, 10, 8), title: "Trip", price: "$200" },
+    { id: 3, date: new Date(2022, 10, 1), title: "Rent", price: "$600" },
   ]);
 
   function deleteHandler(id) {
     const newExpenses = [...expenses].filter((expense) => expense.id !== id);
+    console.log(newExpenses);
+    setExpenses(newExpenses);
+  }
+
+  function editHandler(id) {
+    const newExpenses = [...expenses];
+    newExpenses.map((expense) => {
+      if (expense.id == id) {
+        expense.price = "$100";
+      }
+    });
     console.log(newExpenses);
     setExpenses(newExpenses);
   }
@@ -21,6 +32,7 @@ function Expenses() {
       date={expense.date}
       title={expense.title}
       price={expense.price}
+      edit={() => editHandler(expense.id)}
       delete={() => deleteHandler(expense.id)}
     />
   ));
